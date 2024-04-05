3b83e53752fd93c702f2cf19a25b5d1e7a32a947
Fabrice <aubertf@hotmail.com>
2024-04-05 12:22:13 +0400

    Final

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo apt-get install poppler-utils
-gcloud config set project testfab-362608
+gcloud config set project <projectID>
 gcloud auth application-default login
-gcloud auth application-default set-quota-project testfab-362608
+gcloud auth application-default set-quota-project <projectID>
 
-## Create a service account with Storage Admin permission on the storage bucket
+## Create a service account with Storage Admin permission on the storage bucket (te create SAS signature aginst the storage)
